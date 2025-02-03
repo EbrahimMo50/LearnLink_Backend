fa7db321d1ec7dfda1e40086d62199a4048e19b1
Ebrahim Mohsen <ibrahimjarb69@gmail.com>
2025-02-03 15:56:24 +0200

    tests are prepared and ready to be carried
    got the tests up and running testing the components will be followed by integrity test

---
+++ LearnLink_Backend.Tests/Components/Announcements/AnnounementRepoTests.cs
@@ -0,0 +1,26 @@
+﻿using LearnLink_Backend.Modules.Announcement.Repo;
+using LearnLink_Backend.Modules.Announcement;
+using LearnLink_Backend.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnLink_Backend.Tests.Components.Announcements
+{
+    [TestClass]
+    public class AnnounementRepoTests
+    {
+        [TestMethod]
+        public void CreateAnnouncement_ValidInput_AnnouncementCreated()     // following the [UnitOfWork_StateUnderTest_ExpectedBehavior] convention
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase("TestDatabase")
+            .Options;
+
+            using (var context = new AppDbContext(options)) // Context created inside using
+            {
+                var repo = new AnnouncementRepo(context);
+                var result = repo.CreateAnnouncement(new AnnouncementModel() { Title = "hello", CreatedBy = "self", Description = "this is hello" });
+                Assert.AreEqual(result.Result.Title, "hello");
+            }
+        }
+    }
+}
