0a23c85668a4b0519b5cbd95cd11447cf7eb1d53
Ebrahim Mohsen <ibrahimjarb69@gmail.com>
2025-02-03 10:41:13 +0200

    instructor Id now comes from context

--- LearnLink_Backend/Modules/User/DTOs/ScheduleSet.cs
+++ LearnLink_Backend/Modules/User/DTOs/ScheduleSet.cs
@@ -4,12 +4,16 @@ namespace LearnLink_Backend.Modules.User.DTOs
 {
     public class ScheduleSet
     {
-        public string InstructorId { get; set; }
+        private string InstructorId;
         [Range(0, 6)]
         public List<int> AvilableDays { get; set; } = []; // this will have values from 0 to 6
         [Range(0, 24)]
         public int StartHour { get; set; }
         [Range(0, 24)]
         public int EndHour { get; set; }                  // from 0 to 24
+        public void SetInstructorId(string id)
+        {
+            InstructorId = id;
+        }
     }
 }
