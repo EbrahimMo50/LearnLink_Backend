ffab5f8716b92a33e5207d8c258fae94a7af58a9
Ebrahim Mohsen <ibrahimjarb69@gmail.com>
2025-02-13 15:41:30 +0200

    added more tests for the controller

--- LearnLink_Backend.Tests/Components/Announcements/AnnouncementControllerTests.cs
+++ LearnLink_Backend.Tests/Components/Announcements/AnnouncementControllerTests.cs
@@ -1,4 +1,10 @@
-﻿using Moq;
+﻿using LearnLink_Backend.Controllers;
+using LearnLink_Backend.DTOs;
+using LearnLink_Backend.Services.AnnouncementsService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Security.Claims;
 
 namespace LearnLink_Backend.Tests.Components.Announcements
 {
@@ -6,9 +12,21 @@ namespace LearnLink_Backend.Tests.Components.Announcements
     public class AnnouncementControllerTests
     {
         [TestMethod]
-        public void CreateAnnouncement_ValidInput_AnnouncementCreated()
+        public async Task CreateAnnouncement_IssuerMissing_BadRequestReturned()
         {
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var mockService = new Mock<IAnnouncementService>();
+            var mockRequest = new Mock<HttpContext>();
+            var user = new Mock<ClaimsPrincipal>();
 
+            mockRequest.Setup(x => x.User).Returns(user.Object);
+            mockHttpContextAccessor.Setup(x => x.HttpContext).Returns(mockRequest.Object);
+            mockService.Setup(x => x.CreateAnnouncementAsync(It.IsAny<AnnouncementSet>(), It.IsAny<int>(), It.IsAny<string>())).ReturnsAsync(new Entities.AnnouncementModel());
+
+            var controller = new AnnouncementController(mockService.Object, mockHttpContextAccessor.Object);
+
+            var result = await controller.CreateAnnouncement(new AnnouncementSet(), 1);
+            Assert.IsInstanceOfType<BadRequestObjectResult>(result);
         }
     }
 }
